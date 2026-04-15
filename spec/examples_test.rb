495f2bd7f363f8e167ad495d3dde36d484bf633f
gnuduncan <gnuduncan@gmail.com>
2026-04-15 13:00:23 +0000

    docs: add runnable checkpoint resume example

--- spec/examples_test.rb
+++ spec/examples_test.rb
@@ -6,14 +6,7 @@ require "tmpdir"
 
 class ExamplesTest < Minitest::Test
   def test_workflow_runner_example_executes_successfully
-    env = {
-      "TMPDIR" => Dir.tmpdir,
-      "HOME" => ENV.fetch("HOME", Dir.pwd)
-    }
-
-    stdout, stderr, status = Open3.capture3(env,
-      Gem.ruby, "-Ilib", "examples/workflow_runner.rb",
-      chdir: File.expand_path("..", __dir__))
+    stdout, stderr, status = run_example("examples/workflow_runner.rb")
 
     assert status.success?, <<~MSG
       expected workflow_runner example to succeed
@@ -26,5 +19,34 @@ class ExamplesTest < Minitest::Test
     assert_includes stdout, "=== Manual Workflow ==="
     assert_includes stdout, "=== YAML Workflow ==="
     assert_includes stdout, "=== from_hash Workflow ==="
+  end
+
+  def test_checkpoint_resume_example_executes_successfully
+    stdout, stderr, status = run_example("examples/checkpoint_resume.rb")
+
+    assert status.success?, <<~MSG
+      expected checkpoint_resume example to succeed
+      stdout:
+      #{stdout}
+      stderr:
+      #{stderr}
+    MSG
+
+    assert_includes stdout, "=== First Run ==="
+    assert_includes stdout, "=== Second Run (resume) ==="
+    assert_includes stdout, "Fetch calls: 1"
+  end
+
+  private
+
+  def run_example(path)
+    env = {
+      "TMPDIR" => Dir.tmpdir,
+      "HOME" => ENV.fetch("HOME", Dir.pwd)
+    }
+
+    Open3.capture3(env,
+      Gem.ruby, "-Ilib", path,
+      chdir: File.expand_path("..", __dir__))
   end
 end
