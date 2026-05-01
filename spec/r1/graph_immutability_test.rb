e2644cd16ded3bdf014186de72a86c1d1b691fd7
gnuduncan <gnuduncan@gmail.com>
2026-05-01 17:15:39 +0200

    perf(graph): avoid duping frozen adjacency sets

--- spec/r1/graph_immutability_test.rb
+++ spec/r1/graph_immutability_test.rb
@@ -17,4 +17,28 @@ class GraphImmutabilityTest < Minitest::Test
     assert_raises(FrozenError) { layers.first << :x }
     assert_equal [[:a], [:b]], graph.topological_layers
   end
+
+  def test_frozen_graph_neighbor_queries_return_frozen_sets
+    graph = DAG::Graph.new
+      .add_node(:a)
+      .add_node(:b)
+      .add_edge(:a, :b)
+      .freeze
+
+    assert graph.successors(:a).frozen?
+    assert graph.predecessors(:b).frozen?
+    assert_raises(FrozenError) { graph.successors(:a) << :x }
+  end
+
+  def test_unfrozen_graph_neighbor_queries_return_defensive_copies
+    graph = DAG::Graph.new
+      .add_node(:a)
+      .add_node(:b)
+      .add_edge(:a, :b)
+
+    successors = graph.successors(:a)
+    successors << :x
+
+    refute_includes graph.successors(:a), :x
+  end
 end
